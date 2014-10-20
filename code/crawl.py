d9721518ce78146120ac8414f24486fbdc988c28
David Branner <brannerchinese@gmail.com>
2014-10-19 23:32:31 -0400

    add to-do comments and interwiki stub

--- code/crawl.py
+++ code/crawl.py
@@ -8,6 +8,12 @@
 # To-do: Links containing "redlink=1" link to pages that do not yet exist. 
 # Are we sure they are not being scraped?
 
+# Do logging of errors
+
+# Add all data to .gitignore — transfer by sftp.
+
+# Implement interlanguage links.
+
 import utils as U
 import scrape as S
 import os
